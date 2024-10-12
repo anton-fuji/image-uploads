9cae5d7141c995680ccb194001c5ac1f8b14f753
Fuji <jifu7833@gmail.com>
2024-10-12 21:31:01 +0900

    修正

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def allowed_file(filename):
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MBまで許可
+
 
 @app.route('/upload', methods=['POST'])
 def upload_media():
